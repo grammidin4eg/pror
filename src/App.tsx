bbd1a3823daf9982733b03a6474da6a4ef079029
grammidin4eg@gmail.com <grammidin4eg@gmail.com>
2022-02-15 22:30:36 +0300

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {firebase_app} from './data/firebase';
 
 function App() {
+  console.log('!!! app', firebase_app);
   return (
     <div>
       APP
+      {process.env.REACT_APP_FIREBASE_API_KEY}
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'development' | 'production' | 'test'
+        PUBLIC_URL: string
+        REACT_APP_FIREBASE_API_KEY: string
+        REACT_APP_FIREBASE_AUTH_DOMAIN: string
+        REACT_APP_FIREBASE_PROJECT_ID: string
+        REACT_APP_FIREBASE_STORAGE_BUCKET: string
+        REACT_APP_FIREBASE_SENDER_ID: string
+        REACT_APP_FIREBASE_APP_ID: string
+        }
+    }
+    interface Window {
+        Stripe: any
+    }
