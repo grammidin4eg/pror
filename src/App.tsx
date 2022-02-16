6411df036bf11cc9e3208f844c2842f94d836d4a
grammidin4eg@gmail.com <grammidin4eg@gmail.com>
2022-02-16 23:32:47 +0300

    router and empty pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {firebase_app} from './data/firebase';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { NoPage } from './pages/NoPage';
+import { Signup } from './pages/Signup';
 
 function App() {
   console.log('!!! app', firebase_app);
   return (
-    <div>
-      APP
-      {process.env.REACT_APP_FIREBASE_API_KEY}
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='*' element={<NoPage/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
